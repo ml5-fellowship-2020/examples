ce38ba23768dc261a77aa79c6da64a78529839e6
Yining Shi <shiyining1023@gmail.com>
2020-05-26 02:47:34 -0400

    Updates

---
+++ precache-manifest.744ef21af11e9c97831d546a5e76fb0a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4db2c91dab73b296c29724eb70e0de69",
+    "url": "/examples/index.html"
+  },
+  {
+    "revision": "063a1a02e06d6cf993c5",
+    "url": "/examples/static/css/2.63b65d4e.chunk.css"
+  },
+  {
+    "revision": "9f22baae291887db923c",
+    "url": "/examples/static/css/main.5fb16b6d.chunk.css"
+  },
+  {
+    "revision": "063a1a02e06d6cf993c5",
+    "url": "/examples/static/js/2.1b4f15b3.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/examples/static/js/2.1b4f15b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f22baae291887db923c",
+    "url": "/examples/static/js/main.e8b69e4a.chunk.js"
+  },
+  {
+    "revision": "4b8cb5e6b0ef1b5657ce",
+    "url": "/examples/static/js/runtime-main.ec9bd2e8.js"
+  },
+  {
+    "revision": "f30725d2353d73168cc2652b5cb59cee",
+    "url": "/examples/static/media/coco-ssd-advanced.f30725d2.png"
+  },
+  {
+    "revision": "88e082b8dc81d8729f95bcc092db90c5",
+    "url": "/examples/static/media/ml5_logo.88e082b8.png"
+  },
+  {
+    "revision": "d96eca35ca3b17a93e3ce298c2063a59",
+    "url": "/examples/static/media/placeholder-image.d96eca35.png"
+  },
+  {
+    "revision": "45d82cedb22992f04500015b6dfb1885",
+    "url": "/examples/static/media/word2vec-advanced.45d82ced.png"
+  }
+]);
